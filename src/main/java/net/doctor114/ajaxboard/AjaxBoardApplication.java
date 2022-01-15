81e830a9c843a569fa6e0960b4b3148d10d36a4b
holykhd <holykhd@naver.com>
2022-01-15 18:31:54 +0900

    project setting

---
+++ src/main/java/net/doctor114/ajaxboard/AjaxBoardApplication.java
@@ -0,0 +1,13 @@
+package net.doctor114.ajaxboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/doctor114/ajaxboard/AjaxBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.doctor114.ajaxboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
